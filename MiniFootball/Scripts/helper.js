c90bce74b7201062ff705aaa87c1d5f0fb6a7ce0
unuscontraomnes <vladimir.brezgunov@hotmail.com>
2016-10-11 15:45:51 +0300

    Added Pitch view

--- MiniFootball/Scripts/helper.js
+++ MiniFootball/Scripts/helper.js
@@ -1,3 +1,30 @@
-﻿$(function () {
+﻿// Calendar
+$(function () {
 	$('.datepicker').datepicker();
 });
+
+
+// Google Maps
+$(document).ready(function () {
+	Initialize();
+});
+
+function Initialize() {
+
+	google.maps.visualRefresh = true;
+	var fitnessHouse = new google.maps.LatLng(59.961397, 30.286913);
+
+	var mapOptions = {
+		zoom: 14,
+		center: fitnessHouse,
+		mapTypeId: google.maps.MapTypeId.G_NORMAL_MAP
+	};
+
+	var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+
+	var marker = new google.maps.Marker({
+		position: fitnessHouse,
+		map: map,
+		title: 'Our pitch'
+	});
+}
